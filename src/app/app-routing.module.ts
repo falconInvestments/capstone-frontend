3d0eb1814af49fd75262b5bfdda986657483bad6
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-01-25 11:22:44 -0600

    update styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CertificatesListComponent } from './certificates-list/certificates-list.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'certificates-list', component: CertificatesListComponent },
