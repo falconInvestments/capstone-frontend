4187658dde9b2b7f241cc6d648b8aedb56a4179b
Lance Wolfe <lwolfe@talentpath.com>
2022-01-26 14:27:05 -0500

    Tweak form service

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -3,8 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const baseUrl = 'https://falconInvestments.herokuapp.com';
-// FOR TESTING //
-// const baseUrl = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root',
@@ -12,22 +10,19 @@ const baseUrl = 'https://falconInvestments.herokuapp.com';
 export class FormService {
   constructor(private http: HttpClient) {}
 
-  submitSignup(formValues: any): Observable<any> {
-    const newUser = {
-      firstName: formValues.firstName,
-      lastName: formValues.lastName,
-      email: formValues.email,
-      password: formValues.password,
-    };
-
-    return this.http.post(baseUrl + '/signup', newUser);
+  submitSignup(formValues: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  }): Observable<any> {
+    return this.http.post(baseUrl + '/signup', formValues);
   }
 
-  submitSignin(formValues: any): Observable<any> {
-    const userCredentials = {
-      email: formValues.email,
-      password: formValues.password,
-    };
-    return this.http.post(baseUrl + '/signin', userCredentials);
+  submitSignin(formValues: {
+    email: string;
+    password: string;
+  }): Observable<any> {
+    return this.http.post(baseUrl + '/signin', formValues);
   }
 }
